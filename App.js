7e2717ffd1f905350c844f2202d1f78cdb95d680
Senal Dolage <sdolage@smashydesign.com>
2021-08-28 21:17:30 +0530

    test context added

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import MainStackNavigator from "./navigation/Navigator";
 import { NavigationContainer } from "@react-navigation/native";
+import ItemContext from "./context/ItemContext";
 
 function App() {
   return (
-    <NavigationContainer>
-      <MainStackNavigator />
-    </NavigationContainer>
+    <ItemContext.Provider>
+      <NavigationContainer>
+        <MainStackNavigator />
+      </NavigationContainer>
+    </ItemContext.Provider>
   );
 }
 
---
+++ context/ItemContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const ItemContext = React.createContext("item");
+export default ItemContext;
